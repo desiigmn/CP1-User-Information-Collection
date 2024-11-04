418c76bdccfd35e9bc910f6b89b96f78134068e8
Desiree Ann Guzman <desireeann000@gmail.com>
2024-11-04 17:16:50 +0800

    Commit with userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Desiree Ann P. Guzman";
+let age = 19;
+let favoriteNumber = 12;
+let favoriteColor = "White";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
